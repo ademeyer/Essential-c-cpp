924f88d79b0c3458ec46e37dd659971539f0be60
Adebay Timileyin <adebayotimileyin@gmail.com>
2025-04-23 19:07:56 +0200

    add final interview question solution

--- mainTest/main.c
+++ mainTest/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct data
 {
@@ -7,43 +8,72 @@ typedef struct data
     int value;
 }data_t;
 
+int cmp(const void* a, const void* b)
+{
+    	const data_t* int_a = (const data_t*)a;
+    	const data_t* int_b = (const data_t*)b;
+    
+    	// Safe comparison that avoids overflow
+    	if (int_a->value < int_b->value) return -1;
+    	if (int_a->value > int_b->value) return 1;
+    	return 0;
+}
+
 int min_pieces(int* original, int* desired, int length)
 {
-    int count = 0;
-    const int id = length;
-    
-    data_t valIdx1 [id];
-    data_t valIdx2 [id];
-    
-    for(int i = 0; i < length; ++i)
-    {
-        data_t t1 = {i , original[i]};
-        data_t t2 = {i , desired[i]};
-        valIdx1[i] = t1;
-        valIdx2[i] = t2;
-    }
+    	const int id = length;   
+    	data_t valIdx1 [id];
+    	data_t valIdx2 [id];
+    	
+	// Bind array's value with index.
+    	for(int i = 0; i < length; ++i)
+    	{
+        	data_t t1 = {i , original[i]};
+        	data_t t2 = {i , desired[i]};
+        	valIdx1[i] = t1;
+        	valIdx2[i] = t2;
+    	}
+        
+    	// Sort valIdx1 and valIdx2 using .value
+    	qsort(&valIdx1, length, sizeof(data_t), cmp);
+    	qsort(&valIdx2, length, sizeof(data_t), cmp);
+    	
+	// Allocate space for tracking diff of sorted valIdx1[&2]
+    	int* set = malloc(sizeof(int) * length);
+    	int idx = 0;
+	
+	// Get diff in sorted valIdxs' indexes
+	for(int i = 0; i < length; ++i)
+	{
+		data_t dt1 = valIdx1[i];
+		data_t dt2 = valIdx2[i];
+		int diff = dt1.index - dt2.index;
+		if(idx > 0 && set[idx - 1] == diff)
+			continue;
+		else
+			set[idx++] = diff;
+	}
+    	
+	// Free allocations
+	free(set);
 
-    for(int i = 0; i < length; ++i)
-    {
-        data_t t1 = valIdx1[i];
-        data_t t2 = valIdx2[i];
-        if(t1.index != t2.index && t1.value != t2.value)
-        ++count;
-    }
-    // {1, 4, 3, 2} - original
-    // {1, 2, 4, 3} - desired
-    // {0, 1}, {1, 4} {2, 3} {3, 2}
-    // {0, 1}, {1, 2} {2, 4} {3, 3}
-
-    return count;
+    	return idx;
 }
 
 #ifndef RunTests
 int main()
 {
-    int original[] = {1, 4, 3, 2};
-    int desired[] = {1, 2, 4, 3};
-    printf("%d\n", min_pieces(original, desired, 4));
+    int original1[] = {1, 4, 3, 2};
+    int desired1[] = {1, 2, 4, 3};
+    printf("%d\n", min_pieces(original1, desired1, 4));
+
+    int original2[] = {1, 4, 3, 2, 9, 8};
+    int desired2[] = {8, 1, 9, 4, 3, 2};
+    printf("%d\n", min_pieces(original2, desired2, 6));
+
+    int original3[] = {1, 4, 3, 2, 9, 8};
+    int desired3[] = {8, 1, 9, 3, 4, 2};
+    printf("%d\n", min_pieces(original3, desired3, 6));
 
     return 0;
 }
