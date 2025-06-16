44394a1e572c39cdf6e7bcb59d12c9d2013a3977
Adebay Timileyin <adebayotimileyin@gmail.com>
2025-06-16 00:18:08 -0600

    add test-gorilla dir

---
+++ test-gorilla/moving_average.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void cal_mov_avg(int size, int vec[], int window_size, int* n, int** result)
+{
+  if(window_size > size) return;
+
+  *n = 0;
+  int cum = 0;
+  for(int i = 0; i < size; ++i) // {1, 2, 3, 4, 5}
+  {
+    cum += vec[i];
+    if((i + 1) >= window_size)
+    {
+      (*result)[*n] = round(cum / window_size);
+      ++*n;
+      cum -= vec[i + 1 - window_size]; // remove the head;
+    }
+  }
+}
+
+int main()
+{
+  int* result = (int*)malloc(20 * sizeof(int));
+  int n = 0;
+  
+  int arr[] = {1,2,3,4,5,6,8};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int window_size = 2;
+
+  cal_mov_avg(size, arr, window_size, &n, &result);
+  for(int i = 0; i < n; ++i)
+    printf("%d, ", result[i]);
+  printf("\n");
+
+  free(result);
+
+  return 0;
+}
