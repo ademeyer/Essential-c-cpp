51301a52a8f68f4e695d19b753c7b11d65809ca9
Adebayo Timileyin <adebayotimileyin@gmail.com>
2026-01-15 11:41:15 -0700

    add reverse color test

--- reverse_color.c
+++ reverse_color.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+
 #define R_MASK 0b1111100000000000
 #define G_MASK 0b0000011111100000
 #define B_MASK 0b0000000000011111
@@ -9,8 +11,10 @@ void reverse_color(void *color)
 {
   if (!color)
     return;
-  uint16_t mColor = *((uint16_t *)color);
+
+  uint16_t mColor;
   // Need to handle misalign binary
+  memcpy(&mColor, color, sizeof(uint16_t));
   // Get red value
   uint8_t red = ((mColor & R_MASK) >> 11);
   // Get green value
@@ -26,7 +30,7 @@ int main()
 {
   // 10000 111000 00111
   // 00111 111000 10000
-  uint16_t color1 = 34567, color2 = 6792;
+  uint32_t color1 = 34567, color2 = 6792;
   printf("color1: %d, color2: %d\n", color1, color2);
   reverse_color(&color1);
   reverse_color(&color2);
